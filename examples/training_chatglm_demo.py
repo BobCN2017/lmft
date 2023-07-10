cefa7d832b99e02749bb7556895c95110c1ae9fe
zhangsongbo <zhangp365@gmail.com>
2023-07-10 20:37:01 +0800

    模型切到2

--- examples/training_chatglm_demo.py
+++ examples/training_chatglm_demo.py
@@ -38,10 +38,10 @@ def finetune_demo():
     parser.add_argument('--train_file', default='data/train.tsv', type=str, help='Training data file')
     parser.add_argument('--test_file', default='data/test.tsv', type=str, help='Test data file')
     parser.add_argument('--model_type', default='chatglm', type=str, help='Transformers model type')
-    parser.add_argument('--model_name', default='/content/chatglm-6b', type=str, help='Transformers model or path')
+    parser.add_argument('--model_name', default='/content/chatglm2-6b', type=str, help='Transformers model or path')
     parser.add_argument('--do_train', action='store_true', help='Whether to run training.')
     parser.add_argument('--do_predict', action='store_true', help='Whether to run predict.')
-    parser.add_argument('--output_dir', default='/content/gdrive/MyDrive/chatglm/outputs/' + time_str, type=str,
+    parser.add_argument('--output_dir', default='/content/gdrive/MyDrive/chatglm2/outputs/' + time_str, type=str,
                         help='Model output directory')
     parser.add_argument('--max_seq_length', default=256, type=int, help='Input max sequence length')
     parser.add_argument('--max_length', default=256, type=int, help='Output max sequence length')
